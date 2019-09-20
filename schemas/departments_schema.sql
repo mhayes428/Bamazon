d24a38ccba1df3be7278756ad13eaf534bd0dbf6
Mike Hayes <fikes428@gmail.com>
2019-09-20 18:33:39 -0400

    DEPARTMENTS TABLE ADDED

--- schemas/departments_schema.sql
+++ schemas/departments_schema.sql
@@ -1,5 +1,8 @@
 USE bamazon;
 
 CREATE TABLE departments (
-    department_id INTEGER(200)
-)
+    department_id INTEGER(200) NOT NULL AUTO_INCREMENT,
+    department_name VARCHAR(200) NOT NULL,
+    overhead_cost DECIMAL(10,2) NOT NULL,
+    primary key (department_id)
+);
