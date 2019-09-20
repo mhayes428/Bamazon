641869ab189c3fc414b6e979f419df698e88643e
Mike Hayes <fikes428@gmail.com>
2019-09-20 18:37:17 -0400

    overhead costs might try later

--- schemas/departments_schema.sql
+++ schemas/departments_schema.sql
@@ -1,8 +1,11 @@
-USE bamazon;
+-- USE bamazon;
 
-CREATE TABLE departments (
-    department_id INTEGER(200) NOT NULL AUTO_INCREMENT,
-    department_name VARCHAR(200) NOT NULL,
-    overhead_cost DECIMAL(10,2) NOT NULL,
-    primary key (department_id)
-);
+-- CREATE TABLE departments (
+--     department_id INTEGER(200) NOT NULL AUTO_INCREMENT,
+--     department_name VARCHAR(200) NOT NULL,
+--     overhead_cost DECIMAL(10,2) NOT NULL,
+--     primary key (department_id)
+-- );
+
+-- INSERT INTO departments (department_name, overhead_cost)
+-- VALUES ('Video Games', 10,000)
