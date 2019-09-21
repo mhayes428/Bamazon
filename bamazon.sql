ef29055a0a64618a39c33850afa1b8035a823abf
Mike Hayes <fikes428@gmail.com>
2019-09-21 19:58:25 -0400

    add bamazon sql--products, price, department

---
+++ bamazon.sql
@@ -0,0 +1,42 @@
+CREATE DATABASE bamazon;
+
+  USE bamazon;
+
+CREATE TABLE products (
+  item_id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(50) NULL,
+  department_name VARCHAR(50) NULL,
+  price FLOAT NULL,
+  stock_quantity INT NULL,
+  PRIMARY KEY (item_id)
+);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+      VALUE("World of Warcraft","Video Games", 59.99, 23);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+  VALUE("Madden NFL 20","Video Games", 59.99, 56);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+  VALUE("No Mans Sky","Video Games", 350.00, 43);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+  VALUE("Medieval Total War II","Video Games", 19.99, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+  VALUE("NHL 20","Video Games", 59.99, 59);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+  VALUE("Pokémon: Sword and Shield","Video Games", 59.99, 26);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+  VALUE("NBA 2k20","Video Games", 59.99, 34);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+  VALUE("Call of Duty: Modern Warfare","Video Games", 59.99, 70);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+  VALUE("Elder Scrolls: Skyrim","Video Games", 49.99, 51);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+  VALUE("Pokémon Stadium","Video Games", 29.99, 3);
---
+++ bamazonCustomer.js
@@ -0,0 +1,102 @@
+//NPM required packages 
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var Table = require("cli-table");
+
+//MySQL connection information
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 8889,
+    user: "root",
+    password: "",
+    database: "bamazon"
+
+});
+
+//Connection with DB
+connection.connect(function(err) {
+
+    if (err) throw err;
+
+    console.log("Connected as ID: " + connection.threadId);
+
+});
+
+//Function to start bamazon and display products
+var displayProducts = function() {
+
+    console.log("Welcome to Bamazon! Please, find our inventory bellow.");
+
+    //Query DB
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+
+        // Creates a table
+        var table = new Table({
+            head: ["Item ID", "Product Name", "Department", "Price", "Stock Quantity"]
+        });
+
+
+        // Displays all items for sale
+        for (var i = 0; i < res.length; i++) {
+            table.push([res[i].item_id, res[i].product_name, res[i].department_name,
+                res[i].price, res[i].stock_quantity
+            ]);
+        }
+        console.log(table.toString());
+
+        //Prompt to buy products
+        inquirer.prompt([{
+            name: "itemId",
+            type: "input",
+            message: "Please, type the Item ID you would like to purchase?",
+            validate: function(value) {
+                if (isNaN(value) === false) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }, {
+            name: "quantity",
+            type: "input",
+            message: "How many of this product would you like to buy?",
+            validate: function(value) {
+             if (isNaN(value) === false) {
+                 return true;
+             } else {
+                 return false;
+             }
+            }
+        }]).then(function(answers) {
+            var chosenId = answers.itemId;
+            var chosenQuantity = answers.quantity;
+            purchase(chosenId, chosenQuantity);
+        });
+    });
+};
+///Take order and update DB
+function purchase(ID, quantityNeeded) {
+
+    connection.query("SELECT * FROM products WHERE item_id = " + ID, function(err, res) {
+        if (err) throw err;
+        //
+        if (quantityNeeded <= res[0].stock_quantity) {
+            var totalCost = res[0].price * quantityNeeded;
+
+            console.log("Your total is $" + totalCost + ". Thank you for your purchase!");
+
+            //Update quantity in the DB
+            connection.query("UPDATE products SET stock_quantity = stock_quantity - " + quantityNeeded + " WHERE item_id = " + ID);
+        
+        } else {
+
+            console.log("We don't have enough of that item to fulfill your order.");
+        };
+        //Callback to displayProducts function.
+        displayProducts();
+    })
+}
+
+//Callback to displayProducts function.
+displayProducts();
