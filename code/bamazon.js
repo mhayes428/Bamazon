4fe1d3b43b920a863992498613653529d38d2898
Mike Hayes <fikes428@gmail.com>
2019-09-21 18:01:44 -0400

    format and clean up code

--- code/bamazon.js
+++ code/bamazon.js
@@ -6,10 +6,10 @@ const customer = require('./bamazonCustomer.js');
 const manager = require('./bamazonManager.js');
 const supervisor = require('./bamazonSupervisor.js');
 const bold = chalk.green.bold; // chalk npm--colors
-const table = new Table({ 
+const table = new Table({
     head: [bold('Id'), bold('Product Name'), bold('Department Name'), bold('Price'), bold('Quantity')],
     colWidths: [5, 40, 30, 15, 10], // column width
-    colAligns: ['', '', '', 'right', 'right'], 
+    colAligns: ['', '', '', 'right', 'right'],
     style: { 'padding-top': 100 }
 });
 
@@ -29,10 +29,35 @@ const options = () => {
                     customer();
                 });
                 break;
-                default:
-                    console.log(chalk`{bold.green Have a Great Day!}`);
-                    con.end();
-                    break;
-            };
-        });
+            default:
+                console.log(chalk`{bold.green Have a Great Day!}`);
+                con.end();
+                break;
+        };
+    });
 };
+
+const showAll = cb => {
+
+    con.query('SELECT * FROM products', (err, res) => {
+        if (err) throw err;
+        res.forEach(element => {
+
+            let id = element.item_id;
+            let name = element.product_name;
+            let department = element.department_name;
+            let price = element.price;
+            let stock = element.stock_quantity;
+
+            table.push([id, name, department, price, stock]);
+
+            console.log(chalk`{yellow ${table.toString()}}`);
+            cb();
+        });
+    }
+
+options();
+
+
+    module.exports.showAll = showAll;
+    module.exports.options = options;
