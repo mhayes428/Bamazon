5915450f24417983e7accfec674af41bdad32cdb
Mike Hayes <fikes428@gmail.com>
2019-09-21 17:40:38 -0400

    made constant vars for node modules + npms

---
+++ code/bamazon.js
@@ -0,0 +1,14 @@
+const inquirer = require('inquirer');
+const con = require('./connection.js');
+const Table = require('cli-table-redemption');
+const chalk = require('chalk');
+const customer = require('./bamazonCustomer.js');
+const manager = require('./bamazonManager.js');
+const supervisor = require('./bamazonSupervisor.js');
+const bold = chalk.green.bold; // chalk npm--colors
+const table = new Table({ 
+    head: [bold('Id'), bold('Product Name'), bold('Department Name'), bold('Price'), bold('Quantity')],
+    colWidths: [5, 40, 30, 15, 10], // column width
+    colAligns: ['', '', '', 'right', 'right'], 
+    style: { 'padding-top': 100 }
+});
