4bfe4b1a59d995a5640fcd0ab125140698332294
Mike Hayes <fikes428@gmail.com>
2019-09-21 21:23:42 -0400

    add const port/fix quotation in sql

--- bamazon.sql
+++ bamazon.sql
@@ -12,31 +12,31 @@ CREATE TABLE products (
 );
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-      VALUE("World of Warcraft","Video Games", 59.99, 23);
+      VALUE("World of Warcraft", "Video Games", 59.99, 23);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-  VALUE("Madden NFL 20","Video Games", 59.99, 56);
+  VALUE("Madden NFL 20", "Video Games", 59.99, 56);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-  VALUE("No Mans Sky","Video Games", 350.00, 43);
+  VALUE("No Mans Sky", "Video Games", 350.00, 43);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-  VALUE("Medieval Total War II","Video Games", 19.99, 10);
+  VALUE("Medieval Total War II", "Video Games", 19.99, 10);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-  VALUE("NHL 20","Video Games", 59.99, 59);
+  VALUE("NHL 20", "Video Games", 59.99, 59);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-  VALUE("Pokémon: Sword and Shield","Video Games", 59.99, 26);
+  VALUE("Pokémon: Sword and Shield", "Video Games", 59.99, 26);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-  VALUE("NBA 2k20","Video Games", 59.99, 34);
+  VALUE("NBA 2k20", "Video Games", 59.99, 34);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-  VALUE("Call of Duty: Modern Warfare","Video Games", 59.99, 70);
+  VALUE("Call of Duty: Modern Warfare", "Video Games", 59.99, 70);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-  VALUE("Elder Scrolls: Skyrim","Video Games", 49.99, 51);
+  VALUE("Elder Scrolls: Skyrim", "Video Games", 49.99, 51);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-  VALUE("Pokémon Stadium","Video Games", 29.99, 3);
+  VALUE("Pokémon Stadium", "Video Games", 29.99, 3);
